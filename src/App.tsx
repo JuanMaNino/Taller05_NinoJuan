0263a2cd7e8ce5f7367438b13f2fba77300b75a8
jnino12 <jnino12@cuc.edu.co>
2024-09-19 12:00:42 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import milogo from "./assets/img/Cristiano.png"
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={milogo} alt="Logo de react" />
+      <h1>Cristiano Ronaldo el mejor Jugador del </h1>
+      <p>El mejor momento de la carrera del cristiano fue en el real madrid</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
